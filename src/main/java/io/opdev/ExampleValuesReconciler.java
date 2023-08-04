e3e8894420b6afe1a6ba8f396de341915ee3637c
Sid Kattoju <skattoju@redhat.com>
2023-08-04 16:57:40 -0400

    modelling values as Map<String, Object> and using snake yaml

--- src/main/java/io/opdev/ExampleValuesReconciler.java
+++ src/main/java/io/opdev/ExampleValuesReconciler.java
@@ -3,12 +3,9 @@ package io.opdev;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -16,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
@@ -24,6 +22,7 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import org.yaml.snakeyaml.Yaml;
 
 
 public class ExampleValuesReconciler implements Reconciler<ExampleValues> { 
@@ -35,40 +34,53 @@ public class ExampleValuesReconciler implements Reconciler<ExampleValues> {
 
   private static final Logger log = LoggerFactory.getLogger(ExampleValuesReconciler.class);
 
-  private final String pathToChart = "/deployments/example-chart";
-  private final String outputDir = "/deployments/helm-output";
-  private final String valuesYaml = "/deployments/userValues.yaml";
+  private static final String PATH_TO_CHART = "deployments/example-chart";
+  private static final String OUTPUT_DIR = "deployments/helm-output";
+  private static final String VALUES_YAML = "deployments/userValues.yaml";
 
   @Override
   public UpdateControl<ExampleValues> reconcile(ExampleValues resource, Context<ExampleValues> context) throws FileNotFoundException {
     try {
-        writeValuesYaml(resource.getSpec());
+        writeValuesYaml(resource);
         parseTemplates(resource.getMetadata().getName());
     } catch (Exception e) {
        log.error("parse templates failed!", e);
     }
     
-    File helmOutputDirectory = new File(outputDir + "/example-chart/templates");
+    File helmOutputDirectory = new File(OUTPUT_DIR + "/example-chart/templates");
     File[] files = helmOutputDirectory.listFiles((pathname) -> pathname.getName().endsWith(".yaml"));
 
     for (File yaml : files){
-
        createFromYaml(yaml.getAbsolutePath(), resource);
-
     }
     
     return UpdateControl.noUpdate();
   }
 
-  private void writeValuesYaml(ExampleValuesSpec spec) throws IOException {
-    String yamlRepresentation = spec.toString();
-    Path path = Paths.get(valuesYaml).toAbsolutePath();
-    Files.writeString(path, yamlRepresentation, StandardCharsets.UTF_8);
+  private void writeValuesYaml(ExampleValues resource) throws IOException {
+    validateParsableResource(resource);
+    // We can surpress the cast, since assertions were true
+    @SuppressWarnings("unchecked") 
+    Map<String,Object> valuesMap = (Map<String,Object>) resource.getAdditionalProperties().get("spec");
+    Yaml valuesYaml = new Yaml();
+    Path path = Paths.get(VALUES_YAML).toAbsolutePath();
+    log.info("Parsed the following values from spec : " + valuesYaml.dump(valuesMap));
+    Files.writeString(path, valuesYaml.dump(valuesMap), StandardCharsets.UTF_8);
+  }
+
+  private void validateParsableResource(ExampleValues resource) throws AssertionError {
+    // Assert a spec exists in the resource
+    assert resource.getAdditionalProperties() != null
+           && !resource.getAdditionalProperties().isEmpty()
+           && resource.getAdditionalProperties().containsKey("spec") : "Null or invalid CR provided " + resource.getMetadata().getName();
+    // Assert the spec is a valid parsable mapping
+    Object specMap = resource.getAdditionalProperties().get("spec");
+    assert specMap != null && specMap instanceof Map<?,?> : "Invalid resource spec " + resource.getMetadata().getName();
   }
 
   private void parseTemplates(String releaseName) throws IOException, InterruptedException {
-    log.info("Running helm template to parse " + pathToChart + " and saving output to " + outputDir);
-    Process helmTemplateProcess = new ProcessBuilder().inheritIO().command("helm", "template", releaseName, pathToChart, "-f", valuesYaml, "--output-dir", outputDir).start();
+    log.info("Running helm template to parse " + PATH_TO_CHART + " and saving output to " + OUTPUT_DIR);
+    Process helmTemplateProcess = new ProcessBuilder().inheritIO().command("helm", "template", releaseName, PATH_TO_CHART, "-f", VALUES_YAML, "--output-dir", OUTPUT_DIR).start();
     helmTemplateProcess.waitFor();
 
   }
--- src/main/java/io/opdev/ExampleValuesSpec.java
+++ src/main/java/io/opdev/ExampleValuesSpec.java
@@ -1,24 +1,5 @@
 package io.opdev;
 
-import io.fabric8.kubernetes.client.utils.Serialization;
-
 public class ExampleValuesSpec {
 
-    private Integer replicaCount;
-
-    public Integer getReplicaCount() {
-        return replicaCount;
-    }
-
-    public void setReplicaCount(Integer replicaCount) {
-        this.replicaCount = replicaCount;
-    }
-
-    /**
-     * Returns a yaml representation string of ExampleValuesSpec
-     */
-    @Override
-    public String toString(){
-        return Serialization.asYaml(this);
-    }
 }
